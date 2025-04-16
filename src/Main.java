64875442f0f99692b5c4a7b55da699ad354a1ffa
SaniaMB <saniabhandari05@gmail.com>
2025-04-16 14:55:24 +0530

    Add Main.java

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("""
+                1.Add a task
+                2.Delete a task
+                3.Check pending tasks
+                4.Exit""");
+
+        System.out.print("Enter Your choice:");
+        int choice = scanner.nextInt();
+
+        switch (choice){
+            case 1 ->
+        }
+
+
+    }
+}
